decf2a7ebd5088b7b27ff9f3bcbba497eb6ad139
Ayomide Onalaja <tuf22861@temple.edu>
2018-08-03 09:38:38 -0400

    Added sections

---
+++ src/components/ChooseSection.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="columns">
+        <div class="column content">
+            <h5>Select area of Interest</h5>
+            <select>
+                <option v-for="sec in sections" :value="sec">
+                    {{sec}}
+                </option>
+            </select>
+            <button class="button is-small is-rounded is-info" @click="handleRetrieve">Retrieve</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            sections: {
+                type: Array
+            }
+        },
+        methods: {
+            handleRetrieve() {
+                this.$emit('retrieve', this.sec);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h5 {
+        color: #fff;
+        margin-bottom: 5px;
+    }
+</style>
---
+++ src/resources/sections.js
@@ -0,0 +1,4 @@
+export default `home, arts, automobiles, books, business, fashion, food,
+health, insider, magazine, movies, national, nyregion, obituaries, opinion, politics, 
+realestate, science, sports, sundayreview, technology, theater, tmagazine, 
+travel, upshot, world`
